dd2047a33a26e5b7e6d74eb974b7f6448e9fd1c4
FireFlame135 <mittony@byu.edu>
2025-06-11 01:13:18 -0600

    Updated vite.config.js to include base path for GitHub repo

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-// import Sitemap from 'vite-plugin-sitemap';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/PortfolioWebsite/',  // <-- this is the critical fix
   server: {
     host: "::",
     port: 8080,
   },
-  // plugins: [
-  //   react(),
-  //   mode === 'development' &&
-  //   // Sitemap({ hostname: 'http:////192.168.86.24:8080/' }),
-  // ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
