fc06708ad0b763b362e905b98098738e8e930631
FireFlame135 <mittony@byu.edu>
2025-06-12 15:51:57 -0600

    testing updateing workflow

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -15,7 +15,7 @@ const NotFound = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-800">
       <div className="text-center">
         <h1 className="text-4xl text-gray-900 dark:text-gray-100 font-bold mb-4">404 Error</h1>
-        <p className="text-xl text-gray-600 dark:text-gray-400 mb-4">Oops! Page not found</p>
+        <p className="text-xl text-gray-600 dark:text-gray-400 mb-4">Oops! Page not found.</p>
         <a href="/" className="text-blue-500 dark:text-blue-400 hover:text-blue-700 underline">
           Return to Home
         </a>
