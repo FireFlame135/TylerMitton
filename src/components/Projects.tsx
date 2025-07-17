f79de658642df266ba5a46689919b85c66a93122
FireFlame135 <mittony@byu.edu>
2025-07-16 22:22:31 -0600

    Fixed alignment of elements

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -54,7 +54,7 @@ const Projects = () => {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-12">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-12 items-start">
         {projects.map((project, index) => (
           <ProjectCard key={index} {...project} />
         ))}
