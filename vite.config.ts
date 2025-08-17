b22758f12cf620481c19504d61701be45679dab9
FireFlame135 <mittony@byu.edu>
2025-08-16 21:57:41 -0600

    performance improvements

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite"; 
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ mode }) => ({
-  base: '/TylerMitton/', // possibly add conditional logic for build vs dev
+  base: '/TylerMitton/', 
   server: {
     host: "::",
     port: 8080,
@@ -13,4 +14,21 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.origin === 'https://fireflame135.github.io',
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'my-site-assets',
+              expiration: { maxEntries: 100, maxAgeSeconds: 60 * 60 * 24 * 90 }, // 90 days
+            },
+          },
+        ],
+      },
+    }),
+  ],
 }));
