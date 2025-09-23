756d8ff1e69b0d293978c91a6ad2c266904557b3
FireFlame135 <mittony@byu.edu>
2025-09-23 16:39:32 -0600

    Hopefully lower FCP and render blocking

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,4 @@
-// import { defineConfig } from "vite"; 
-// import react from "@vitejs/plugin-react-swc";
-// import path from "path";
-// import { VitePWA } from 'vite-plugin-pwa'
 
-// export default defineConfig(({ mode }) => ({
-//   base: '/TylerMitton/', 
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-//   plugins: [
-//     VitePWA({
-//       registerType: 'autoUpdate',
-//       workbox: {
-//         runtimeCaching: [
-//           {
-//             urlPattern: ({ url }) => url.origin === 'https://fireflame135.github.io',
-//             handler: 'CacheFirst',
-//             options: {
-//               cacheName: 'my-site-assets',
-//               expiration: { maxEntries: 100, maxAgeSeconds: 60 * 60 * 24 * 90 }, // 90 days
-//             },
-//           },
-//         ],
-//       },
-//     }),
-//   ],
-// }));
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
