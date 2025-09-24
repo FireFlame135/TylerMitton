8efbd71e0c467b47f341955ccd35b2f2b76e1e1b
FireFlame135 <mittony@byu.edu>
2025-09-24 17:49:49 -0600

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-console.log("🚀 main.tsx loaded BEFORE all code");
-
-
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+console.log("🚀 main.tsx loaded BEFORE all code");
+console.log("React:", React);
 
+createRoot(document.getElementById("root")!).render(<App />);
 console.log("🚀 main.tsx loaded AFTER ALL CODE");
