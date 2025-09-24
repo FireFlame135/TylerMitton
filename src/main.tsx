21bb41f7308aaac3a7d992da24eb932d3ad328f2
FireFlame135 <mittony@byu.edu>
2025-09-24 17:46:34 -0600

    revert and console log

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
+console.log("🚀 main.tsx loaded BEFORE all code");
+
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+console.log("🚀 main.tsx loaded AFTER ALL CODE");
