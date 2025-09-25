fb298b0f9d061203372876b723e0a1cc0252ae0a
FireFlame135 <mittony@byu.edu>
2025-09-24 20:34:51 -0600

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 import Critters from "critters";
 
 const crittersWhitelist = [
