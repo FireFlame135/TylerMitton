5068da8b8f05701e218df322072c6fc49d707fca
FireFlame135 <mittony@byu.edu>
2025-09-24 20:26:41 -0600

    .

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-console.log("🚀 main.tsx loaded BEFORE all code");
-console.log("React:", React);
-
 createRoot(document.getElementById("root")!).render(<App />);
-console.log("🚀 main.tsx loaded AFTER ALL CODE");
