a86d71734c4b76b7b910cee573c28a9e1dfc8bf9
FireFlame135 <FireFlame135@users.noreply.github.com>
2026-01-14 05:09:13 +0000

    deploy: 0e989f93a6b7a63bed0da189420de20a1b0f44ff

---
+++ assets/LinkedIn-CWQ0oyeI.js
@@ -0,0 +1,2 @@
+import{r as t,j as e}from"./index-CNWXpGJw.js";function i(){return t.useEffect(()=>{window.location.replace("https://www.linkedin.com/in/tyler-mitton")},[]),e.jsx("p",{children:"Redirecting to LinkedIn..."})}export{i as default};
+//# sourceMappingURL=LinkedIn-CWQ0oyeI.js.map
