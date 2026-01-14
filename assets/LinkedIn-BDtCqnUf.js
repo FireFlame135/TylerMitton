80e3c9228f5876fe9c84b51f4d87d091e7f10183
FireFlame135 <FireFlame135@users.noreply.github.com>
2026-01-14 06:47:37 +0000

    deploy: 4dcfe038e915dff4405da87be62e334fc9592c2c

---
+++ assets/LinkedIn-BDtCqnUf.js
@@ -0,0 +1,2 @@
+import{r as t,j as e}from"./index-DfK3SehC.js";function i(){return t.useEffect(()=>{window.location.replace("https://www.linkedin.com/in/tyler-mitton")},[]),e.jsx("p",{children:"Redirecting to LinkedIn..."})}export{i as default};
+//# sourceMappingURL=LinkedIn-BDtCqnUf.js.map
--- assets/ScrollToTop-CjROE6u1.js
+++
@@ -1,7 +0,0 @@
-import{r as s,j as o,B as i}from"./index-CNWXpGJw.js";import{c as a}from"./Footer-D7uDjwih.js";/**
- * @license lucide-react v0.462.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const n=a("ChevronUp",[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]]),g=()=>{const[t,r]=s.useState(!1);s.useEffect(()=>{const e=()=>{window.scrollY>300?r(!0):r(!1)};return window.addEventListener("scroll",e),()=>window.removeEventListener("scroll",e)},[]);const l=()=>{window.scrollTo({top:0,behavior:"smooth"})};return o.jsx(o.Fragment,{children:t&&o.jsx(i,{onClick:l,className:"fixed bottom-8 right-8 z-50 h-12 w-12 rounded-full shadow-lg bg-gray-600 hover:bg-gray-900 dark:bg-gray-700 dark:hover:bg-gray-500 transition-all duration-300","aria-label":"Scroll to top",children:o.jsx(n,{size:24})})})};export{g as S};
-//# sourceMappingURL=ScrollToTop-CjROE6u1.js.map
